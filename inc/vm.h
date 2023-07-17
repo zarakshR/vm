f6db86460fdedf74eaac7e0b33f9731b3791057d
Zaraksh Rahman <zarakshR@pm.me>
2023-07-17 17:05:28 +0100

    Remove unused includes

--- inc/vm.h
+++ inc/vm.h
@@ -1,6 +1,8 @@
 #ifndef VM__VM_H
 #define VM__VM_H
 
+#include <stdlib.h>
+
 #include "types.h"
 #include "loader_dumper.h"
 #include "interpreter.h"
--- src/vm.c
+++ src/vm.c
@@ -1,7 +1,3 @@
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 #include "vm.h"
 
 State state;
